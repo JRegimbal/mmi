5b04f8b986bf9fa6f0b40603be7cd6457c16bd75
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-10-31 21:07:53 -0400

    Add an interpolate external

--- include/externals.h
+++ include/externals.h
@@ -4,6 +4,7 @@
  */
 
 #include <flext.h>
+#include "interpolation.h"
 
 #if !defined(FLEXT_VERSION) || (FLEXT_VERSION < 400)
 #error You need at least flext version 0.4.0
@@ -27,12 +28,34 @@ class weights : public flext_base {
 
 FLEXT_LIB_1("weights", weights, float);
 
+class interpolate : public flext_base {
+    FLEXT_HEADER(interpolate, flext_base)
+  public:
+    interpolate(float shape);
+
+  protected:
+    void m_interpolate(int argc, t_atom *argv);
+    void m_weight(int argc, t_atom *argv);
+
+    float shape;
+    Eigen::MatrixXd weights;
+    std::vector<Interpol::ControlPoint> points;
+
+  private:
+    FLEXT_CALLBACK_V(m_interpolate);
+    FLEXT_CALLBACK_V(m_weight);
+};
+
+FLEXT_LIB_1("interpolate", interpolate, float);
+
+
 static void lib_setup() {
     post("mmi lib (C)2020 Juliette Regimbal");
-    post("weights");
+    post("weights, interpolate");
     post("");
 
     FLEXT_SETUP(weights);
+    FLEXT_SETUP(interpolate);
 }
 
 FLEXT_LIB_SETUP(mmi, lib_setup);
--- src/externals.cpp
+++ src/externals.cpp
@@ -64,3 +64,71 @@ void weights::m_run(int argc, t_atom *argv) {
 }
 
 void weights::m_shape(float f) { shape = f; }
+
+interpolate::interpolate(float shape) : shape(shape) {
+    AddInList("List containing 2D point to interpolate from.");
+    AddInList("List containing formatted control point data.");
+    AddOutList("Interpolated vector as a message.");
+
+    FLEXT_ADDMETHOD(0, m_interpolate);
+    FLEXT_ADDMETHOD(1, m_weight);
+}
+
+void interpolate::m_weight(int argc, t_atom *argv) {
+    if (argc < 2) {
+        post("%s - Size of inputs not specified!", thisName());
+        return;
+    }
+    int number = GetAInt(argv[0]);
+    int size = GetAInt(argv[1]);
+
+    // Check if total size matches expected.
+    if (argc != number * size + 2) {
+        post("%s - Expected a message of length %d but got %d!", thisName(),
+             number * size + 2, argc);
+        return;
+    }
+
+    // Start loading in values...
+    points.clear();
+    for (int i = 2; i < argc; i += size) {
+        ControlPoint p;
+        p.visual(0) = GetAFloat(argv[i]);
+        p.visual(1) = GetAFloat(argv[i + 1]);
+        if (p.control.size() < size - 2) {
+            p.control.resize(size - 2);
+        }
+        for (int j = 2; j < size; j++) {
+            p.control(j - 2) = GetAFloat(argv[i + j]);
+        }
+        points.push_back(p);
+    }
+
+    MatrixXd im = Gaussian::InterpolationMatrix(points, shape);
+    MatrixXd controls = ControlMatrix(points);
+    weights = SolveWeights(im, controls);
+}
+
+void interpolate::m_interpolate(int argc, t_atom *argv) {
+    if (argc != 2) {
+        post("%s - 2 values were expected, received %d!", thisName(), argc);
+        return;
+    }
+
+    if (weights.size() == 0 || points.empty()) {
+        post("%s - The control values are not set. Can't interpolate!", thisName());
+        return;
+    }
+
+    Vector2d p;
+    p << GetAFloat(argv[0]), GetAFloat(argv[1]);
+    VectorXd interpolated = Gaussian::Interpolate(weights, p, points, shape);
+
+    int size = interpolated.size();
+    t_atom *list = new t_atom[size];
+    for (int i = 0; i < size; i++) {
+        SetFloat(list[i], interpolated[i]);
+    }
+
+    ToOutList(0, size, list);
+}
