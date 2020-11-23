70c84c6e65eebb3f5e4f0d37fe976ff5510ebf71
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-11-23 11:15:47 -0500

    Find Pd path as well

--- cmake/FindFlext.cmake
+++ cmake/FindFlext.cmake
@@ -9,6 +9,7 @@ set(FLEXT_SEARCH_PATHS
     /opt/local
     /opt
     ${FLEXT_PATH}
+    ${PD_PATH}
 )
 
 find_library(Flext_LIBRARY
@@ -22,7 +23,12 @@ find_path(Flext_INCLUDE_DIR flext.h
     PATHS ${FLEXT_SEARCH_PATHS}
 )
 
+find_path(Flext_PD_INCLUDE m_pd.h
+    PATH_SUFFIXES include include/pd src *
+    PATHS ${FLEXT_SEARCH_PATHS}
+)
+
 message("End FindFlext.cmake")
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Flext Flext_LIBRARY Flext_INCLUDE_DIR)
+find_package_handle_standard_args(Flext Flext_LIBRARY Flext_INCLUDE_DIR Flext_PD_INCLUDE)
